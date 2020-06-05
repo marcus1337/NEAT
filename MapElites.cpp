26070be7ffe6796f1ade0b2376517f7431eb802f
marcus1337 <blurgas@hotmail.com>
2020-06-05 13:06:47 +0200

    map elites

---
+++ MapElites.cpp
@@ -0,0 +1,89 @@
+
+#include "MapElites.h"
+
+#include <functional>
+#include <iterator> 
+#include "Utils.h"
+
+using namespace NTE;
+
+
+bool MapElites::isOccupied(std::tuple<int, int, int> key) {
+    std::map<std::tuple<int, int, int>, NEAT>::iterator it = eliteNEATs.find(key);
+    if (it != eliteNEATs.end())
+        return true;
+    return false;
+}
+
+bool MapElites::isOccupied(int a, int b, int c) {
+    std::map<std::tuple<int, int, int>, NEAT>::iterator it = eliteNEATs.find(std::make_tuple(a, b, c));
+    if (it != eliteNEATs.end())
+        return true;
+    return false;
+}
+
+bool MapElites::isEliteBetter(int a, int b, int c, NEAT& NEAT) {
+    return isOccupied(a, b, c) && eliteNEATs[getKey(NEAT)].fitness > NEAT.fitness;
+}
+
+void MapElites::storeNEAT(int a, int b, int c, NEAT& newNEAT) {
+    eliteNEATs[std::make_tuple(a, b, c)] = newNEAT;
+}
+
+NEAT MapElites::getNEAT(int a, int b, int c) {
+    return eliteNEATs[std::make_tuple(a, b, c)];
+}
+
+void MapElites::mapOrStoreElite(NEAT& NEAT) {
+    auto _key = getKey(NEAT);
+    if (!isOccupied(_key) || isNewNEATBetter(_key, NEAT))
+        eliteNEATs[_key] = NEAT;
+    else
+        NEAT = eliteNEATs[_key];
+}
+
+bool MapElites::isNewNEATBetter(std::tuple<int, int, int> _key, NEAT& NEAT) {
+    return eliteNEATs[_key].fitness < NEAT.fitness
+        || (eliteNEATs[_key].fitness == NEAT.fitness);
+}
+
+void MapElites::mapOrStoreElites(std::vector<NEAT>& NEATs) {
+    for (size_t i = 0; i < NEATs.size(); i++)
+        mapOrStoreElite(NEATs[i]);
+}
+
+
+void MapElites::storeElite(NEAT& NEAT) {
+    auto _key = getKey(NEAT);
+    if (!isOccupied(_key) || isNewNEATBetter(_key, NEAT)) {
+        eliteNEATs[_key] = NEAT;
+    }
+}
+void MapElites::storeElites(std::vector<NEAT>& NEATs) {
+    for (size_t i = 0; i < NEATs.size(); i++)
+        storeElite(NEATs[i]);
+}
+
+std::vector<NEAT> MapElites::getElitesVector() {
+    std::vector<NEAT> allElites;
+    std::transform(eliteNEATs.begin(), eliteNEATs.end(), std::back_inserter(allElites),
+        [](auto &kv) { return kv.second; });
+    return allElites;
+}
+
+void MapElites::randomElitism(std::vector<NEAT>& NEATs) {
+    if (eliteNEATs.empty())
+        return;
+    std::vector<NEAT> allElites = getElitesVector();
+    for (size_t i = 0; i < NEATs.size(); i++) {
+        int randomEliteIndex = Utils::randi(0, allElites.size() - 1);
+        NEATs[i] = allElites[randomEliteIndex];
+    }
+}
+
+std::tuple<int, int, int> MapElites::getKey(NEAT& NEAT) {
+    int a = NEAT.observedBehaviors[0];
+    int b = NEAT.observedBehaviors[1];
+    int c = NEAT.observedBehaviors[2];
+    return std::make_tuple(a, b, c);
+}
---
+++ MapElites.h
@@ -0,0 +1,41 @@
+
+#include <vector>
+#include "NEAT.h"
+
+#include <map>
+#include <algorithm>
+#include <vector>
+#include <tuple>
+
+#include <string>
+#include <unordered_map>
+
+#ifndef MAPELITES_H
+#define MAPELITES_H
+
+namespace NTE {
+    class MapElites {
+
+    public:
+        std::map<std::tuple<int, int, int>, NEAT> eliteNEATs;
+
+        bool isOccupied(std::tuple<int, int, int> key);
+        bool isOccupied(int a, int b, int c);
+        bool isEliteBetter(int a, int b, int c, NEAT& newNEAT);
+        void storeNEAT(int a, int b, int c, NEAT& newNEAT);
+        NEAT getNEAT(int a, int b, int c);
+
+        void mapOrStoreElite(NEAT& oldNEAT);
+        void mapOrStoreElites(std::vector<NEAT>& NEATs);
+        bool isNewNEATBetter(std::tuple<int, int, int> key, NEAT& newNEAT);
+
+        std::vector<NEAT> getElitesVector();
+
+        void storeElite(NEAT& NEAT);
+        void storeElites(std::vector<NEAT>& NEATs);
+        std::tuple<int, int, int> getKey(NEAT& NEAT);
+        void randomElitism(std::vector<NEAT>& NEATs);
+    };
+}
+
+#endif
